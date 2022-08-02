ba069d0fdf91b383604fd3f4c44a321cbbe63c64
YoungJae Kim <byeongal@comcom.ai>
2022-08-02 14:57:55 +0900

    add api to get server status

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,7 +1,22 @@
-from fastapi import APIRouter
+import os
+
+from fastapi import APIRouter, Request
+
+from config import ainetwork_settings
+from schemas import ServerStatus
 
 from . import v1
 
 
 router = APIRouter()
 router.include_router(v1.router, prefix="/v1")
+
+
+@router.get("/")
+async def get_server_status(request: Request) -> ServerStatus:
+    bots = request.app.state.bots
+    return ServerStatus(
+        number_of_workers=os.getenv("NUMBER_OF_WORKERS", 1),
+        number_of_bot=len(list(bots.keys())),
+        ain_provider_url=ainetwork_settings.ain_provider_url,
+    )
